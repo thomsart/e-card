3aba2dc130bbff9b0f246f0c02e15053c05cfd30
thomsart <psychoid@hotmail.fr>
2022-09-27 12:14:30 +0200

    presntation cards OK

--- card/models.py
+++ card/models.py
@@ -5,11 +5,12 @@ from django.contrib.auth.models import User
 
 class Card(models.Model):
     created_at = models.DateField(auto_now_add=True)
-    profession = models.CharField(max_length=20, null=False, default="profession")
+    profession = models.CharField(max_length=30, null=False, default="profession")
+    text = models.TextField(max_length=200, null=True, blank=True)
     phone = models.CharField(max_length=15, null=False, default="0646756938")
     email = models.EmailField(max_length=254)
     photo = models.ImageField(max_length=20, null=True, blank=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     class Meta():
-        ordering = ['user']
+        ordering = ['-user']
